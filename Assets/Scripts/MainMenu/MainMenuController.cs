76b7e51619d5535e530eea9686d56410ae361ee3
Nathan <maraval.nathan@gmail.com>
2020-03-12 09:07:15 +0100

    refacto menu principal

--- Assets/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Scripts/MainMenu/MainMenuController.cs
@@ -7,23 +7,68 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
- 
- 
-    public void playGame() {
-        SceneManager.LoadScene("gameScene");
+    public GameObject gameModeMenu;
+    public GameObject selectCharacterMenu;
+
+    // Main menu
+
+    public void gameMode()
+    {
+        mainMenu.SetActive(false);
+        gameModeMenu.SetActive(true);
     }
- 
+
     public void options() {
         mainMenu.SetActive(false);
         optionsMenu.SetActive(true);
     }
  
-    public void back() {
-        mainMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-    }
- 
     public void exitGame() {
         Application.Quit();
     }
+
+    // Options
+
+    public void backOption()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+    // GameMode Selection
+
+    public void fastPlay()
+    {
+        gameModeMenu.SetActive(false);
+        selectCharacterMenu.SetActive(true);
+    }
+
+    public void draftMode()
+    {
+        gameModeMenu.SetActive(false);
+        //selectCharacterMenu.SetActive(true);
+    }
+
+    public void backGameMode()
+    {
+        mainMenu.SetActive(true);
+        gameModeMenu.SetActive(false);
+    }
+
+    // Select Character
+
+    public void fighter()
+    {
+        StaticCharacters.character1 = createFighter.createFighter(CharacterSide.Left);
+        StaticCharacters.character2 = createFighter.createFighter(CharacterSide.Right);
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void backSelectCharacter()
+    {
+        mainMenu.SetActive(true);
+        selectCharacterMenu.SetActive(false);
+    }
+
 }
